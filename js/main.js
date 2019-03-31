7272033237aa93355683821d1eac19065c0ae5a1
Tejas Sadrani <tejas0904@gmail.com>
2019-03-31 04:57:14 -0400

    my commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-
 (function ($) {
     "use strict";
-    let registerForm =  $('#register-form');
+    let registerForm = $('#register-form');
     $('#jerseySize').parent().append('<ul class="list-item" id="newjerseySize" name="jerseySize"></ul>');
     $('#jerseySize option').each(function () {
         $('#newjerseySize').append('<li value="' + $(this).val() + '">' + $(this).text() + '</li>');
@@ -27,18 +26,13 @@
 //	'50%': [  400, 100 ],
 //	'max': [ 1000 ]
 //}
-    
-    
-    
-    
-    
-    
+
     let battingSlider = document.getElementById('slider-margin');
     if (battingSlider !== undefined) {
         noUiSlider.create(battingSlider, {
             start: [5],
             step: 1,
-            connect: [true,false],
+            connect: [true, false],
             tooltips: [true],
             range: {
                 'min': 0,
@@ -51,11 +45,10 @@
                 thousand: ',',
                 prefix: ' ',
             })
-            
+
 
         });
     }
-
 
     let bowlingSlider = document.getElementById('slider-margin1');
     if (bowlingSlider !== undefined) {
@@ -133,54 +126,83 @@
         equalTo: ""
     });
 
-    function checkForValidations() {
-        //check validations over here, and make this function as Promise
-    }
+    //const URL = "https://cloud4i862354trial.hanatrial.ondemand.com/cloud/webapi/player/playerDetails";
+    const URL = "http://localhost:8080/cloud/webapi/player/playerDetails";
 
-    registerForm.submit(function (event) {
-        event.preventDefault();
-        //const url1 = "http://apl2019.us-east-2.elasticbeanstalk.com/playerdetails";
-       const url = "http://aplreg2019.us-east-2.elasticbeanstalk.com/webapi/player/playerDetails";
+    const checkForValidations = () => {
+        // TODO: check validations over here, and make this function as Promise
+        const jerseyNumber = parseInt(document.getElementById("chequeno").value.trim());
+        if (jerseyNumber < 0 || jerseyNumber > 99) {
+            throw new Error("invalid jersey number");
+        }
+        const mobileNumber = parseInt(document.getElementById("phone_number").value.trim().replace(/-/g, ''));
+        const battingRating = parseInt(battingSlider.noUiSlider.get().trim());
+        const bowlingRating = parseInt(bowlingSlider.noUiSlider.get().trim());
+        const fieldingRating = parseInt(fieldingSlider.noUiSlider.get().trim());
+        //const jerseySizeElement = document.getElementById("jerseySize");
+       //console.log(jerseySizeElement.options[jerseySizeElement.selectedIndex].value);
+    };
 
+    const createData = () => {
+        const jerseyNumber = document.getElementById("chequeno").value.trim();
+        const jerseySizeElement = document.getElementById("jerseySize");
+        const jerseySize = jerseySizeElement.outerText.toLocaleLowerCase().trim();
+        const base64PhotoWithFileExtensionSeperatedByDot = document.getElementById('base64Image').value;
+        const array = base64PhotoWithFileExtensionSeperatedByDot.toString().split('.');
+        document.getElementById('base64Image').value = "";
 
-       checkForValidations();
-
-        const Data = {
+        const requestBody = {
             firstName: document.getElementById("first_name").value.trim(),
             lastName: document.getElementById("last_name").value.trim(),
             email: document.getElementById("email").value.trim(),
-            mobileNumber : parseInt(document.getElementById("phone_number").value.trim().replace(/-/g,'')),   // this line will remove dash in string and convert it to integer
+            mobileNumber: parseInt(document.getElementById("phone_number").value.trim().replace(/-/g, '')),   // this line will remove dash in string and convert it to integer
             streetAddress: document.getElementById("street_number").value.trim() + document.getElementById("route").value.trim(),
             city: document.getElementById("locality").value.trim(),
             state: document.getElementById("administrative_area_level_1").value.trim(),
             zipCode: document.getElementById("postal_code").value.trim(),
             country: document.getElementById("country").value.trim(),
-            jerseyNumber: parseInt(document.getElementById("chequeno").value.trim() === undefined ? 0 : document.getElementById("chequeno").value.trim()),
-            sevaCollector:document.getElementById("locality").value.trim(),
-            jerseySize :document.getElementById("jerseySize").value ===  undefined ? "medium" : document.getElementById("jerseySize").value.trim(),
+            jerseyNumber: jerseyNumber ? parseInt(jerseyNumber) : 0,
+            sevaCollector: document.getElementById("locality").value,
+            jerseySize: jerseySize,
             isPaid: false,
-            photo :"document.getElementByIdfgn.value",
-            battingRating :parseInt(battingSlider.noUiSlider.get().trim()), // getter syntax for the value of slider
-            bowlingRating :parseInt(bowlingSlider.noUiSlider.get().trim()),
-            fieldingRating :parseInt(fieldingSlider.noUiSlider.get().trim()),
-            battingComment :document.getElementById("battingComments").value.trim(),
-            bowlingComment :document.getElementById("bowlingComments").value.trim(),
-            fieldingComment : document.getElementById("fieldingComments").value.trim()
+            photo: array[0],
+            battingRating: parseInt(battingSlider.noUiSlider.get().trim()), // getter syntax for the value of slider
+            bowlingRating: parseInt(bowlingSlider.noUiSlider.get().trim()),
+            fieldingRating: parseInt(fieldingSlider.noUiSlider.get().trim()),
+            battingComment: document.getElementById("battingComments").value.trim(),
+            bowlingComment: document.getElementById("bowlingComments").value.trim(),
+            fieldingComment: document.getElementById("fieldingComments").value.trim(),
+            imageFormat: array[1]
         };
 
-        fetch(url, {
-            headers:{
-                "content-type":"application/json"
+        console.log(requestBody);
+        return requestBody;
+    };
+
+    const sendRequest = () => {
+        fetch(URL, {
+            headers: {
+                "content-type": "application/json"
             },
-            body: JSON.stringify(Data),
-            method:"POST"
+            body: JSON.stringify(createData()),
+            method: "POST"
+        }).then(response => {
+            alert("Successfully submitted! voila! " + response)
+        }).catch(error => {
+            console.log(error);
+            alert("unexpected error occurred. Please try again or contact the admin.");
         })
-            .then(res => {
-                alert("Successfully submitted! voila! " + res)
-            })
-            .catch(error => console.log(error))
+    };
 
+
+    registerForm.submit(event => {
+        try {
+            event.preventDefault();
+            checkForValidations();
+            sendRequest();
+        } catch (error) {
+            alert(error);
+        }
     });
-
 
 })(jQuery);
